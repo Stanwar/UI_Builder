3aba0a2c17a22a92ee6f304a15c87e6268496c0b
stanwar <sharad_tanwar@outlook.com>
2016-05-09 22:29:27 -0500

    deleted examples

--- examples/imariViz/test_CPAR/scripts/app.js
+++
@@ -1,4 +0,0 @@
-var leafletApp = angular.module('leafletApp', ['leaflet-directive','controllers','ngRoute','ui.bootstrap','services','trNgGrid','ngSanitize', 'ngCsv','ui-rangeSlider']);
-//var sidebarApp = angular.module('sidebarApp', []);
-
-
